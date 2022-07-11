ba4562025a90c9a06f43addb435be625e428a812
Pablo López <tak30@users.noreply.github.com>
2022-07-11 09:55:25 +0200

    Fixed elvis warnings and bumped rebar3 version to 3.19.0 for the CI (#52)
    * Fixed elvis warnings and bumped rebar3 version to 3.19.0 for the CI
    
    * Changed function ensure_all_started as it was returning something not useful

--- src/erlmld_app.erl
+++ src/erlmld_app.erl
@@ -2,7 +2,7 @@
 
 -behaviour(application).
 
--export([start/2, stop/1, ensure_all_started/0]).
+-export([start/2, stop/1, ensure_all_deps_started/0]).
 
 %% application callback.  load default configuration from application environment, and
 %% start erlmd_sup with that configuration.
@@ -15,11 +15,7 @@ start(_StartType, []) ->
 stop(_State) ->
     ok.
 
-ensure_all_started() ->
+ensure_all_deps_started() ->
     {ok, Deps} = application:get_key(erlmld, applications),
-    {ok,
-     lists:append([begin
-                       {ok, Started} = application:ensure_all_started(Dep),
-                       Started
-                   end
-                   || Dep <- Deps])}.
+    lists:foreach(fun(Dep) -> {ok, _} = application:ensure_all_started(Dep) end, Deps),
+    ok.
