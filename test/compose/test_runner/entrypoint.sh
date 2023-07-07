5ba28913644fa66f1a6b91af2ef03e543a15344b
Josh Burroughs <josh@qhool.com>
2023-07-07 16:43:34 -0400

    Work around Docker desktop
      Docker desktop uses a utility vm to host the containers.
      In addition to making things slower, this has other advantages:
      for instance, by running as root and mapping file ownership to root,
      it can break workflows that assume the docker user is unpriviledged.

--- test/compose/test_runner/entrypoint.sh
+++ test/compose/test_runner/entrypoint.sh
@@ -41,14 +41,43 @@ aws dynamodb list-tables || exit $?
 #                                         #
 ###########################################
 
-if [ "$(id -u)" = "0" ]; then
-    echo $(ls -nd . | cut -d' ' -f3,4) |
-        while read uid gid
-        do
-            groupadd -g $gid work_user
-            useradd -u $uid -g $gid work_user -d /home/work
-        done
+if [ "$(id -u)" -eq "0" ]; then
+    uid=$(ls -nd ./code | cut -d' ' -f3)
+    gid=$(ls -nd ./code | cut -d' ' -f4)
+    if [ "$uid" -eq "0" -o "$gid" -eq "0" ]; then
+        groupadd work_user
+        useradd -g work_user work_user -d /home/work
+        cp -r /home/work/code /home/work/copy
+        chown -R work_user:work_user /home/work/copy
+        cd /home/work/copy
+        set +x
+        cat <<EOF
+
+*********************************************************************************
+**                                                                             **
+**                            N  O  T  I  C  E                                 **
+**                                                                             **
+** --------------------------------------------------------------------------- **
+**                                                                             **
+** You are running docker as root, and your working dir is owned by root.      **
+** This is probaby due to using docker-desktop (maybe on MacOs or Windows).    **
+**                                                                             **
+** Output files will not be present in your working dir. To retrieve, run:     **
+**                                                                             **
+** docker compose -f test/compose.yml cp tests:/home/work/copy/_build ./_out   **
+**                                                                             **
+*********************************************************************************
+
+EOF
+        set -x
+    else
+        groupadd -g $gid work_user
+        useradd -u $uid -g $gid work_user -d /home/work
+        cd /home/work/code
+    fi
+    chown work_user:work_user /home/work
     # insert gosu command into $@
+    ls -ld /home/work
     set -- gosu work_user "$@"
 fi
 
